a9fdbb7fac7352b38962b6dedb5d07a1b064073c
yaakov04 <dbrionesmtz@yahoo.com>
2021-08-19 13:18:55 -0500

    agregue las funciones para calcular la altura de un triangulo isiceles

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 //calcular el perimetro de un cuadrado
 const perimetroCuadrado = lado => lado * 4;
 //area de un cuadrado
-const areaCuadrado = lado => lado * lado;
+const areaCuadrado = lado => lado ** 2;
 //perimetro de un triangulo
 const perimetroTriangulo = (lado1, lado2, base) => lado1 + lado2 + base;
 //area de un triangulo
@@ -9,4 +9,12 @@ const areaTriangulo = (altura, base) => (altura * base) / 2;
 //perimetro/circunferenci de un circulo (2r*PI)
 const perimetroCirculo = radio => (2 * radio) * Math.PI;
 //Area circulo (pi*r^2)
-const areaCirculo = radio => Math.PI * (radio * radio);
+const areaCirculo = radio => Math.PI * (radio ** 2);
+//altura de un triangulo isoceles
+const alturaTrianguloIsoceles = (lado1, lado2, base) => {
+    if (lado1 == lado2) {
+        return Math.sqrt(lado1 ** 2 - (base ** 2 / 4));
+    } else {
+        return 'No es un triangulo isoceles'
+    }
+}
